12c1060ec820bf468993c9b9f485a6f60e00f24e
Zihan Ye <zihan@iconcreativestudio.com>
2020-04-22 16:01:19 -0700

    worked with reference

--- examples/DictView.py
+++ examples/DictView.py
@@ -8,33 +8,34 @@ class DictModel(QtGui.QStandardItemModel):
     def __init__(self, student_dict=dict()):
         super(DictModel, self).__init__()
         self.student_dict = student_dict
+        self.dict = {}
         self.setup_model()
 
     def setup_model(self):
         for key in self.student_dict:
             key_item = QtGui.QStandardItem(key)
+            model = QtGui.QStandardItemModel()
             self.appendRow(key_item)
+            self.dict[key] = model
             for attr in self.student_dict[key]:
                 value = self.student_dict[key][attr]
                 attr_item = QtGui.QStandardItem(attr)
                 value_item = QtGui.QStandardItem(value)
-                key_item.appendRow([attr_item, value_item])
+                model.appendRow([attr_item, value_item])
+            item = QtGui.QStandardItem()
+            item.setData(model, QtCore.Qt.UserRole)
+            key_item.appendRow(item)
+            print 'aaa', key_item, item, item.data(QtCore.Qt.UserRole)
             
     def data(self, index, role):
         if role == QtCore.Qt.DisplayRole:
             return QtGui.QStandardItemModel.data(self, index, role)
         if role == QtCore.Qt.UserRole:
-            model = QtGui.QStandardItemModel()
-            model.setColumnCount(2)
-            key = index.data()
-            key_item = QtGui.QStandardItem(key)
-            model.appendRow(key_item)
-            for attr in self.student_dict[key]:
-                value = self.student_dict[key][attr]
-                attr_item = QtGui.QStandardItem(attr)
-                value_item = QtGui.QStandardItem(value)
-                key_item.appendRow([attr_item, value_item])
-            return model
+            print '---------------'
+            key_item = self.itemFromIndex(index)
+            item = key_item.child(0, 0)
+            print key_item, item, item.data(role)
+            return item.data(role)
 
 class DictView(QtWidgets.QWidget):
     def __init__(self):
