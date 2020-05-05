f41d2365a0e1b431f3ce90368cdd3afd7b3f2755
Zihan Ye <zihan@iconcreativestudio.com>
2020-05-05 09:32:16 -0700

    updated editing model

--- tree_icon.py
+++ tree_icon.py
@@ -17,11 +17,9 @@ class MyWindow(QtWidgets.QWidget):
         self.proxy_model = QtCore.QSortFilterProxyModel()
         self.proxy_model.setSourceModel(model)
         view.setModel(self.proxy_model)
-        view.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
-        view.show()
 
-        # item_delegate = ItemDelegate(self)
-        # view.setItemDelegate(item_delegate)
+        item_delegate = ItemDelegate(self)
+        view.setItemDelegate(item_delegate)
 
         # latest version is setSectionResizeMode() however, qtpy is loading old version, so use setResizeMode()
         # header_view = view.horizontalHeader()
@@ -49,6 +47,59 @@ class MyWindow(QtWidgets.QWidget):
     def onTextChanged(self, text):
         self.proxy_model.setFilterRegExp(str(text))
 
+class ItemDelegate(QtWidgets.QStyledItemDelegate):
+    def __init__(self, parent=None):
+        super(ItemDelegate, self).__init__(parent)
+        self.edited_indexes = dict()
+
+    def createEditor(self, parent, option, index):
+        editor = None
+        table_model = index.model().sourceModel()
+        column = index.column()
+        if column in [table_model.columnIndex('dest_path')]:
+            editor = QtWidgets.QLineEdit(parent)
+
+        elif column in [table_model.columnIndex('source_role'), table_model.columnIndex('dest_role')]:
+            editor = QtWidgets.QComboBox(parent)
+
+        return editor
+
+    def setEditorData(self, editor, index):
+        proxy_model = index.model()
+        table_model = proxy_model.sourceModel()
+
+        column = index.column()
+        if column in [table_model.columnIndex('dest_path')]:
+            editor.setText(proxy_model.data(index, QtCore.Qt.DisplayRole))
+
+        elif column in [table_model.columnIndex('source_role'), table_model.columnIndex('dest_role')]:
+            sorted_spaces = sorted(table_model.colorspace_mappings.keys())
+            editor.addItems(sorted_spaces)
+            editor.setCurrentIndex(sorted_spaces.index(proxy_model.data(index, QtCore.Qt.DisplayRole)))
+
+        elif column == table_model.columnIndex['depth']:
+            depths = sorted([item['depth'] for key, item in table_model.colorspace_mappings.iteritems()])
+            editor.addItems(depths)
+            editor.setCurrentIndex(depths.index(proxy_model.data(index)))
+
+        else:
+            return None
+
+    def setModelData(self, editor, model, index):
+        table_model = model.sourceModel()
+        column = index.column()
+        column_indexes = table_model.columnIndexes()
+        table_index = model.mapToSource(index)
+        if column in [column_indexes['source_path'], column_indexes['dest_path']]:
+            value = editor.text()
+            table_model.setData(table_index, value)
+        elif column in [column_indexes['source_role'], column_indexes['dest_role']]:
+            value = editor.currentText()
+            table_model.setData(table_index, value)
+
+    def updateEditorGeometry(self, editor, option, index):
+        editor.setGeometry(option.rect)
+
 class TreeModel(QtCore.QAbstractItemModel):
     def __init__(self, parent=None):
         super(TreeModel, self).__init__(parent)
@@ -62,6 +113,20 @@ class TreeModel(QtCore.QAbstractItemModel):
             {'name': 'orig_source', 'visible': False},
             {'name': 'folder', 'visible': False},
             ]
+        self.colorspace_mappings = {'nc8': {'space': 'nc8', 'format': 'uint8'},
+            'nc16': {'space': 'nc16', 'format': 'uint16'},
+            'ncf': {'space': 'ncf', 'format': 'float'},
+            'nch': {'space': 'nch', 'format': 'half'},
+            'dt8': {'space': 'lnh', 'format': 'half'},
+            'dt16': {'space': 'lnh', 'format': 'half'},
+            'dth': {'space': 'lnh', 'format': 'half'},
+            'dtf': {'space': 'lnf', 'format': 'float'},
+            'lnh': {'space': 'lnh', 'format': 'half'},
+            'lnf': {'space': 'lnf', 'format': 'float'},
+            'dtt': {'space': 'lnh', 'format': 'half'},
+            'srgb': {'space': 'lnh', 'format': 'half'},
+            'srgf': {'space': 'lnf', 'format': 'float'}
+            }
         data = [
             {'source_path': 'AOV_spec_dot_nc8.tif', 'dest_role': 'nc8', 'depth': 'uint8', 'source_role': 'nc8',
                 'orig_source': 'AOV_spec_dot_nc8.tif', 'dest_path': 'AOV_spec_dot_nc8.tx',
@@ -99,6 +164,14 @@ class TreeModel(QtCore.QAbstractItemModel):
     def columnCount(self, parent):
         num_visible = len([col for col in self.__columns if col['visible']])
         return num_visible
+    
+    def columnIndex(self, name):
+        for i, col in enumerate(self.__columns):
+            if col['name'] == name:
+                return i
+
+    def columnIndexes(self):
+        return {col['name']: i for i, col in enumerate(self.__columns)}
 
     def data(self, index, role):
         if not index.isValid():
@@ -110,12 +183,20 @@ class TreeModel(QtCore.QAbstractItemModel):
         item = index.internalPointer()
 
         return item.data(index.column())
+    
+    def setData(self, index, value, role=QtCore.Qt.EditRole):
+        if not index.isValid():
+            return False
+
+        if role == QtCore.Qt.EditRole:
+            self.rootItem.child(index.row()).itemData[index.column()] = value
+            return True
 
     def flags(self, index):
         if not index.isValid():
             return QtCore.Qt.NoItemFlags
 
-        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEditable
 
     def headerData(self, col, orientation, role):
         if orientation == QtCore.Qt.Horizontal:
